5b13d5ae773a7984ea7dd54701344f08d1a61fd1
kingmore96 <wgg9696@163.com>
2025-03-30 18:18:07 +0800

    c-primer-plus chap3-增加指针测试代码

---
+++ labs/c-primer-plus/chap3/pointer.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stddef.h>
+
+void test1()
+{
+	int a[2][3] =  {{1,2,3},{4,5,6}};
+	
+	int (*p1)[3] = a;
+	int (*p2)[2][3] = &a;
+
+	printf("access 6 : %d\n",*(*(p1+1)+2));
+	printf("access 6 : %d\n",*(((int*)(p2+1))-1));
+	printf("access 6 : %d\n",*((int*)((char*)(*p2) + sizeof(int) * 5)));
+}
+
+
+void test2()
+{
+	struct Point{
+		int x;
+		char y;
+		double z;
+	};
+	
+	struct Point arr[2] = {{1,'A',3.14},{5,'B',6.28}};
+	struct Point *ptr = arr;
+	struct Point (*ptr1)[2] = &arr;
+
+	printf("access second Point : y = %c, z = %.2f\n",(ptr + 1)->y,*((double*)((char*)ptr + sizeof(struct Point) + offsetof(struct Point,z))));
+	printf("access second Point : y = %c, z = %.2f\n",(*(ptr1 + 1) - 1)->y,*((double*)(ptr1 + 1)-1));
+}
+
+
+void test3()
+{
+	int *arr[2];
+
+	int a[2] = {10,20};
+	int b[3] = {30,40,50};
+
+	arr[1] = a;
+	arr[2] = b;
+
+	printf("access a-20 : %d , access b-40 : %d\n",*(arr[1] + 1),arr[2][1]);
+}
+
+void test4()
+{
+	
+
+
+int main(void)
+{
+	test1();
+	test2();
+	test3();
+	return 0;
+}
+
